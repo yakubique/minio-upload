a68b9910d600c3640c3d516c93b7b34957ec006d
Vladimir Levin <vladimirlevinozinki@gmail.com>
2023-08-06 17:58:00 +0900

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,9 @@ if [[ "$YA_INSECURE" == "true" ]]; then
 fi
 
 recursive_option=""
-if [[ "YA_RECURSIVE" == "true" ]]; then
+if [[ "$YA_RECURSIVE" == "true" ]]; then
   recursive_option="--recursive"
 fi
 
 mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY"
-
 mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
