3afecdfe52c53bab0bc530069996dc2a5f5cd859
Vladimir Levin <vladimirlevinozinki@gmail.com>
2023-08-06 20:34:43 +0900

    Minor fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,5 @@ if [[ "$YA_RECURSIVE" == "true" ]]; then
   recursive_option="--recursive"
 fi
 
-mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY"
-mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
+$HOME/minio-binaries/mc alias set ${insecure_option:+"$insecure_option"} target "$YA_ENDPOINT" "$YA_ACCESS_KEY" "$YA_SECRET_KEY"
+$HOME/minio-binaries/mc ${insecure_option:+"$insecure_option"} cp ${recursive_option:+"$recursive_option"} $1 "target/$2"
